84ab38e43debd376320d1992e97d5ab9094996f8
Bill Hughes <whughesiii@pm.me>
2026-04-05 23:13:30 -0500

    undo git fukkup

--- macos/nvim/.config/nvim/lua/plugins/ef-themes.lua
+++
@@ -1,9 +0,0 @@
-return { 
-  "oonamo/ef-themes.nvim",
-  config = function()
-    require("ef-themes").setup({ 
-      transparent = true 
-    })
-  vim.cmd.colorscheme("ef-deuteranopia-dark")
-  end
-}
