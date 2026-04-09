dfa1eabb410c43900dea5862aa07c6717c681865
Bill Hughes <whughesiii@pm.me>
2026-04-08 23:45:07 -0500

    more minor tweaks, ready for production... I think

--- dotfiles/.config/hypr/scripts/installupdates.sh
+++ dotfiles/.config/hypr/scripts/installupdates.sh
@@ -1,36 +1,36 @@
 #!/usr/bin/env bash
 
-### MOD THIS TO REMOVE FEDORA!! 
+### MOD THIS TO REMOVE FEDORA!!
 
 # Check if command exists
 _checkCommandExists() {
-    cmd="$1"
-    if ! command -v "$cmd" >/dev/null; then
-        echo 1
-        return
-    fi
-    echo 0
-    return
+	cmd="$1"
+	if ! command -v "$cmd" >/dev/null; then
+		echo 1
+		return
+	fi
+	echo 0
+	return
 }
 
 _isInstalled() {
-    package="$1"
-    case $install_platform in
-        arch)
-            check="$($aur_helper -Qs --color always "${package}" | grep "local" | grep "${package} ")"
-            ;;
-        fedora)
-            check="$(dnf repoquery --quiet --installed ""${package}*"")"
-            ;;
-        *) ;;
-    esac
+	package="$1"
+	case $install_platform in
+	arch)
+		check="$($aur_helper -Qs --color always "${package}" | grep "local" | grep "${package} ")"
+		;;
+	fedora)
+		check="$(dnf repoquery --quiet --installed ""${package}*"")"
+		;;
+	*) ;;
+	esac
 
-    if [ -n "${check}" ]; then
-        echo 0 #'0' means 'true' in Bash
-        return #true
-    fi
-    echo 1 #'1' means 'false' in Bash
-    return #false
+	if [ -n "${check}" ]; then
+		echo 0 #'0' means 'true' in Bash
+		return #true
+	fi
+	echo 1 #'1' means 'false' in Bash
+	return #false
 }
 
 # ------------------------------------------------------
@@ -39,43 +39,43 @@ _isInstalled() {
 
 sleep 1
 clear
-figlet -f smslant "Updates"
+figlet -f smslant "System Updates Available"
 echo
 primarycolor=$(cat ~/.config/hypr/colors/primary)
 onsurfacecolor=$(cat ~/.config/hypr/colors/onsurface)
 if gum confirm --selected.background=$primarycolor --prompt.foreground=$onsurfacecolor "DO YOU WANT TO START THE UPDATE NOW?"; then
-    echo
-    echo ":: Update started..."
+	echo
+	echo ":: Update started..."
 elif [ $? -eq 130 ]; then
-    exit 130
+	exit 130
 else
-    echo
-    echo ":: Update canceled."
-    exit
+	echo
+	echo ":: Update canceled."
+	exit
 fi
 
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 # Install update
-# ----------------------------------------------------- 
+# -----------------------------------------------------
 
 # Arch
 if [[ $(_checkCommandExists "pacman") == 0 ]]; then
 
-    yay_installed="false"
-    paru_installed="false"
-    if [[ $(_checkCommandExists "yay") == 0 ]]; then
-        yay_installed="true"
-    fi
-    if [[ $(_checkCommandExists "paru") == 0 ]]; then
-        paru_installed="true"
-    fi
-    if [[ $yay_installed == "true" ]] && [[ $paru_installed == "false" ]]; then
-        yay
-    elif [[ $yay_installed == "false" ]] && [[ $paru_installed == "true" ]]; then
-        paru -Syu --noconfirm
-    else
-        yay
-    fi
+	yay_installed="false"
+	paru_installed="false"
+	if [[ $(_checkCommandExists "yay") == 0 ]]; then
+		yay_installed="true"
+	fi
+	if [[ $(_checkCommandExists "paru") == 0 ]]; then
+		paru_installed="true"
+	fi
+	if [[ $yay_installed == "true" ]] && [[ $paru_installed == "false" ]]; then
+		yay
+	elif [[ $yay_installed == "false" ]] && [[ $paru_installed == "true" ]]; then
+		paru -Syu --noconfirm
+	else
+		yay
+	fi
 fi
 echo
 
